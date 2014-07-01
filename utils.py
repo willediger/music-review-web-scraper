f3300e55f8579edbe8de5ca1faac7542d4516ccf
Will <willhinsa@gmail.com>
2014-06-30 22:15:18 -0500

    both album tables created

--- utils.py
+++ utils.py
@@ -9,6 +9,17 @@ field_type = 'text'
 
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
-c.execute('drop table {tn}'.format(tn=grabbed_albums_tbl))
-c.execute('create table {tn} ({nf} {ft}, {nf2} {ft}, primary key({nf}, {nf2}))'
-          .format(tn=grabbed_albums_tbl, nf = artist_field, ft = field_type, nf2 = album_field))
+
+# c.execute('drop table if exists {tn}'.format(tn=grabbed_albums_tbl))
+# c.execute('create table {tn} ({nf} {ft}, {nf2} {ft}, primary key({nf}, {nf2}))'
+#           .format(tn=grabbed_albums_tbl, nf = artist_field, ft = field_type, nf2 = album_field))
+
+#creates album tables with composite key of artist & album names, given table_name
+def create_album_table(table_name):
+    c.execute('drop table if exists {tn}'.format(tn=table_name))
+    c.execute('create table {tn} ({nf} {ft}, {nf2} {ft}, primary key({nf}, {nf2}))'
+          .format(tn=table_name, nf=artist_field, ft=field_type, nf2=album_field))
+    return True
+
+create_album_table(grabbed_albums_tbl)
+create_album_table(new_albums_tbl)
